824f17185efb456ba23506c8c45e3e2fa23fdf87
Jesús Giganto Diez <Jesus.Giganto@tajamar365.com>
2018-02-22 00:15:43 +0100

    gt

---
+++ MVCAlmacenarconsesionlargo/Controllers/EmpleadosController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCAlmacenarconsesionlargo.Models;
+
+namespace MVCAlmacenarconsesionlargo.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        ModeloHospital modelo;
+        public EmpleadosController()
+        {
+            modelo = new ModeloHospital();
+        }
+        public ActionResult AlmacenarEmpleadosSession(int? empno)
+        {
+            if (empno != null)
+            {
+                List<int> lista;
+                if (Session["EMPLEADOS"] == null)
+                {
+                    lista = new List<int>();
+                }
+                else
+                {
+                    lista = Session["EMPLEADOS"] as List<int>;
+                }
+                lista.Add(empno.GetValueOrDefault());
+                Session["EMPLEADOS"] = lista;
+                ViewBag.Mensaje = "Empleados almacenados: " + lista.Count();
+            }
+            IQueryable empleados = this.modelo.GetEmpleados();
+            return View(empleados);
+        }
+        public ActionResult MostrarEmpleadosSession(int? eliminar)
+        {
+            if (eliminar != null)
+            {
+                List<int> lista = (List<int>)Session["EMPLEADOS"];
+                lista.Remove(eliminar.GetValueOrDefault());
+                if (lista.Count() == 0)
+                {
+                    Session["EMPLEADOS"] = null;
+                }
+                else
+                {
+                    Session["EMPLEADOS"] = lista;
+                }
+            }
+            if (Session["EMPLEADOS"] == null)
+            {
+                return View();
+            }
+            else
+            {
+                List<int> lista = (List<int>)Session["EMPLEADOS"];
+                IQueryable<EMP> empleados = this.modelo.GetEmpleadosSession(lista);
+                return View(empleados);
+            }
+        }
+
+
+        // GET: Empleados
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCAlmacenarconsesionlargo/Models/ModeloHospital.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCAlmacenarconsesionlargo.Models
+{
+    
+    public class ModeloHospital
+    {
+        ContextoHospital contexto;
+        public ModeloHospital()
+        {
+            contexto = new ContextoHospital();
+        }
+        public IQueryable<EMP> GetEmpleados()
+        {
+            var consulta = from datos in this.contexto.EMP
+                           select datos;
+            return consulta;
+        }
+
+        public IQueryable<EMP> GetEmpleadosSession(List<int> emplados)
+        {
+            var consulta = from datos in this.contexto.EMP
+                           where emplados.Contains(datos.EMP_NO)
+                           select datos;
+            return consulta;
+        }
+    }
+}
